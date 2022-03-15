bd1dbab8d7a92fbda4fa0e178b9e7c171ae1ba3a
j0rdann <jordan.c.oldridge@gmail.com>
2022-03-15 20:01:15 +1300

    initial commit after setup

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# bot code, to run use this: pipenv run python main.py
+
+# Installing packages
+from twitchAPI.twitch import Twitch
+import os
+
+# Test function, just to get it working
+def test():
+    print(os.environ.get("BOT_PREFIX"))
+    print("Hi my prefix is: " , os.environ.get("BOT_PREFIX"))
+
+# Main function, everything runs through here
+def main():
+    test()
+
+# Safety function
+if __name__ == "__main__":
+    main()
